9020287b46a34d257a4fe6eea09dcd9be0f9486d
Gabrielsgff <167806793+Gabrielsgff@users.noreply.github.com>
2024-04-26 15:18:04 -0300

    Contador pronto

---
+++ script.js
@@ -0,0 +1,27 @@
+function contar() {
+    let inicio = document.getElementById('inicio')
+    let fim = document.getElementById('fim')
+    let passo = document.getElementById('passo')
+    let saida = document.getElementById('saida')
+    // 
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        alert('ERRO')
+    } else {
+        alert('FOI')
+        saida.innerHTML = `Contando: `
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (i <= f) {
+            for (let c = i; c <=f; c += p) {
+            saida.innerHTML += `${c} ➡️`
+            }
+        } else {
+            for (let c = i; c >= f; c -= p)
+            saida.innerHTML += `${c} ➡️`
+        }
+        saida.innerHTML +=`🚩`
+        
+
+    }
+}
